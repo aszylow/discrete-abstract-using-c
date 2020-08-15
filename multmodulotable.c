96a5be14d10363263b1fcd6585180b6d3d9b825d
Alen Szylowiec <69704122+aszylow@users.noreply.github.com>
2020-08-14 21:28:34 -0700

    Update multmodulotable.c

--- multmodulotable.c
+++ multmodulotable.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 
-//multiplication modulo table
+// Outputting a multiplication group table.
 
-void multtable(int *arr, int modulo) {
+void multtable(int *arr, int len, int modulo) {
 
-    for(int i = 0; i < modulo; i++) {
-        for(int j = 0; j < modulo; j++)
-            printf("%d\t", (arr[j] * i) % modulo);
+    for(int i = 0; i < len; i++) {
+        for(int j = 0; j < len; j++)
+            printf("%d\t", (arr[j] * arr[i]) % modulo);
         printf("\n");
     }
 }
 
+int gcd(int num1, int num2) {
+
+   while(num2 != 0) {
+        int temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+    }
+    return num1;
+}
+
 int main() {
 
-    int modulo = 8;
-    int arr[modulo];
+    int modulo = 10, count = 0;
     for(int i = 0; i < modulo; i++)
-        arr[i] = i;
+        if(gcd(modulo, i) == 1)
+            count++;
 
-    multtable(arr, modulo);
+    int arr[count], index = 0;
+    for(int i = 0; i < modulo; i++)
+        if(gcd(modulo, i) == 1)
+            arr[index++] = i;
+
+    multtable(arr, count, modulo);
 
     return 0;
 }

d3aceddb94db47ae5c0fdd3b58e9914d5f4ab132
Alen Szylowiec <69704122+aszylow@users.noreply.github.com>
2020-08-16 17:00:31 -0700

    Update exponentiation2.c

--- discrete-math/exponentiation2.c
+++ discrete-math/exponentiation2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Finds the remainder of a very large number by modular exponentiation using a linked list.
+// Finds the remainder of a very large integer by modular exponentiation using a linked list.
 
 struct Node {
     int data;
